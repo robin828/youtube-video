8c1c0d09315fe16ec9b2af69d32f7ddb59ec8669
robin828 <robin19093@iiitd.ac.in>
2023-08-01 03:16:04 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const handleClick = () => {
+
+    axios.post('https://accounts.google.com/o/oauth2/v2/auth', {
+        "client_id": "809358982466-20nathtulih1fcreju627tthrma3mban.apps.googleusercontent.com",
+            "redirect_uri": "https://gan.'ai",
+                        "response_type": "token",
+                        "scope": "https://www.googleapis.com/auth/youtube.upload",
+                        "include_granted_scopes": "true",
+                        "state": "pass-through value",
+                        "grant_type": "authorization_code",
+                        "code": "pp"
+           }).then(res=>console.log(res))
+
+
+//     axios.post(`https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDIHZFvfVuM3oBm6bEMb3rDO8ETXu78yjw`, 
+//     {headers: {
+//       'Authorization': 'Bearer [YOUR_ACCESS_TOKEN]',
+// 'Accept': 'application/json',
+// 'Content-Type':'application/json'
+
+    // }})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
